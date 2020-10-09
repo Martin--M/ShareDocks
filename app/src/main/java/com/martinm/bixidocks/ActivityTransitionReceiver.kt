a75777fa8b3d3d8dc6e0f3d851f8580baddb9a35
Martin M <martin.mena@outlook.com>
2020-10-09 17:23:52 -0400

    activity recognition: add broadcaster receiver for activity transitions
    This is basically the class that will be called with a pending intent
    whenever the system detects an activity transition

---
+++ app/src/main/java/com/martinm/bixidocks/ActivityTransitionReceiver.kt
@@ -0,0 +1,26 @@
+package com.martinm.bixidocks
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.google.android.gms.location.ActivityTransition
+import com.google.android.gms.location.ActivityTransitionResult
+import com.google.android.gms.location.DetectedActivity
+
+class ActivityTransitionReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (!ActivityTransitionResult.hasResult(intent)) {
+            return
+        }
+        val result = ActivityTransitionResult.extractResult(intent)!!
+        for (event in result.transitionEvents) {
+            if (event.activityType == DetectedActivity.ON_BICYCLE) {
+                if (event.transitionType == ActivityTransition.ACTIVITY_TRANSITION_ENTER) {
+                    LogicHandler.startTracking()
+                } else if (event.transitionType == ActivityTransition.ACTIVITY_TRANSITION_EXIT) {
+                    LogicHandler.stopTracking()
+                }
+            }
+        }
+    }
+}
