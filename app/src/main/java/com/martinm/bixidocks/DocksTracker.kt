9635587b8b85e0b14aaad47cca6f62fec3df4c4e
Martin M <martin.mena@outlook.com>
2020-10-12 14:03:44 -0400

    tracker: add foreground service class
    This class simply takes care of starting a forground service that will
    be running the update timer in the LogicHandler

---
+++ app/src/main/java/com/martinm/bixidocks/DocksTracker.kt
@@ -0,0 +1,32 @@
+package com.martinm.bixidocks
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class DocksTracker : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent?.action.equals(FOREGROUND_SERVICE_START)) {
+            startForeground(
+                NotificationHandler.NOTIFICATION_ID_TRACKING,
+                NotificationHandler.getForegroundNotification(baseContext)
+            )
+            LogicHandler.startTracking(baseContext)
+            return START_STICKY
+        }
+        // No need to check for FOREGROUND_SERVICE_STOP, just stop for safety
+        LogicHandler.stopTracking()
+        stopForeground(true)
+        stopSelf()
+        return START_NOT_STICKY
+    }
+
+    companion object {
+        const val FOREGROUND_SERVICE_START = "com.martinm.bixidocks.action.starttracking"
+        const val FOREGROUND_SERVICE_STOP = "com.martinm.bixidocks.action.stoptracking"
+    }
+}
