a6bc3f542477f5129f13b205757b05ff50c306d8
Martin M <martin.mena@outlook.com>
2020-10-12 13:48:47 -0400

    notification: add function to get ongoing notification
    This is used to notify the user that the application has launched a
    foreground service.

--- app/src/main/java/com/martinm/bixidocks/NotificationHandler.kt
+++ app/src/main/java/com/martinm/bixidocks/NotificationHandler.kt
@@ -1,5 +1,6 @@
 package com.martinm.bixidocks
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
@@ -8,29 +9,45 @@ import androidx.core.app.NotificationManagerCompat
 
 object NotificationHandler {
     private lateinit var mNotificationManager: NotificationManager
-    private const val mChannelID: String = "DocksUpdates"
-    private lateinit var mBuilder: NotificationCompat.Builder
-    private lateinit var mChannel: NotificationChannel
+
+    private const val CHANNEL_ID_UPDATES: String = "DocksUpdates"
+    private const val CHANNEL_ID_TRACKING: String = "DocksTracking"
 
     fun initialize(notificationManager: NotificationManager) {
         mNotificationManager = notificationManager
-        mChannel =
-            NotificationChannel(mChannelID, "DocksChannel", NotificationManager.IMPORTANCE_HIGH)
-        mChannel.apply {
+        val chanUpdates = NotificationChannel(
+            CHANNEL_ID_UPDATES,
+            "DocksChannel",
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
             description = "DocksDescription"
         }
-        mNotificationManager.createNotificationChannel(mChannel)
+        val chanTracking = NotificationChannel(
+            CHANNEL_ID_TRACKING,
+            "DocksTrackingChannel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = "Channel used for sending tracking notification"
+        }
+        mNotificationManager.createNotificationChannel(chanUpdates)
+        mNotificationManager.createNotificationChannel(chanTracking)
     }
 
     fun showNotification(context: Context, message: String) {
-        mBuilder = NotificationCompat.Builder(context, mChannelID)
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID_UPDATES)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentTitle("Station full")
+            .setContentText(message)
 
-        mBuilder.setContentText(message)
         with(NotificationManagerCompat.from(context)) {
-            notify(100001, mBuilder.build())
+            notify(100001, builder.build())
         }
+    }
+
+    fun getForegroundNotification(context: Context): Notification {
+        return NotificationCompat.Builder(context, CHANNEL_ID_TRACKING)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Station tracking in progress").build()
     }
 }
